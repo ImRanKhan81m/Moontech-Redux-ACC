ea775fd09fb9ce00c8ad29d1803580e8764d199a
Md Imran Hossen <freelancerimran81@gmail.com>
2022-12-08 16:15:30 +0600

    middleware add

--- MoonTech-Client/src/components/ProductCard.js
+++ MoonTech-Client/src/components/ProductCard.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
-import { useProducts } from "../context/ProductProvider";
-import { actionTypes } from "../state/ProductState/actionTypes";
+import { useDispatch } from "react-redux";
+import { addToCart, removeFromCart } from "../redux/actionCreators/productAction";
+import { RiDeleteBin6Line } from 'react-icons/ri';
+import { useLocation } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
-  const { dispatch } = useProducts();
+  const dispatch = useDispatch();
+  const { pathname } = useLocation()
 
   return (
     <div
@@ -24,20 +27,37 @@ const ProductCard = ({ product }) => {
         </ul>
       </div>
       <div className='flex gap-2 mt-5'>
-        <button
-          className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
-          onClick={() =>
-            dispatch({ type: actionTypes.ADD_TO_CART, payload: product })
-          }
-        >
-          Add to cart
-        </button>
-        <button
-          title='Add to wishlist'
-          className='bg-indigo-500  py-1 px-2 rounded-full'
-        >
-          <BiListPlus className='text-white' />
-        </button>
+        {
+          !pathname.includes('cart') &&
+          <button
+            className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
+            onClick={() =>
+              dispatch(addToCart(product))
+            }
+          >
+            Add to cart
+          </button>
+        }
+        {
+          pathname.includes('cart') &&
+          <button
+            className='bg-red-500 rounded-full py-3 px-2 flex-1 text-white text-bold flex justify-center items-center gap-2'
+            onClick={() =>
+              dispatch(removeFromCart(product))
+            }
+          > <p>Remove </p>
+            <RiDeleteBin6Line />
+          </button>
+        }
+        {
+          !pathname.includes('cart') &&
+          <button
+            title='Add to wishlist'
+            className='bg-indigo-500  py-1 px-2 rounded-full'
+          >
+            <BiListPlus className='text-white' />
+          </button>
+        }
       </div>
     </div>
   );
--- MoonTech-Client/src/pages/Home.js
+++ MoonTech-Client/src/pages/Home.js
@@ -1,39 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import ProductCard from "../components/ProductCard";
-import { useProducts } from "../context/ProductProvider";
 
 const Home = () => {
-  // const {
-  //   state: { products, loading, error },
-  // } = useProducts();
+  const [products, setProducts] = useState();
 
-  let content;
+  useEffect(() => {
+    fetch('http://localhost:5000/products')
+      .then(res => res.json())
+      .then(data => setProducts(data))
 
-  // if (loading) {
-  //   content = <p>Loading</p>;
-  // }
+  }, [])
 
-  // if (error) {
-  //   content = <p>Something went wrong</p>;
-  // }
+  // console.log(products.data)
 
-  // if (!loading && !error && products.length === 0) {
-  //   content = <p>Nothing to show, product list is empty</p>;
-  // }
-
-  // if (!loading && !error && products.length) {
-  //   content = products.map((product) => (
-  //     <ProductCard key={product._id} product={product} />
-  //   ));
-  // }
-
-  const state = useSelector ((state) => state);
+  const state = useSelector((state) => state);
   console.log(state);
 
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      {content}
+      {
+        products?.data?.map(product=>{
+          return <ProductCard key={product.id} product={product} />
+        })
+      }
     </div>
   );
 };
