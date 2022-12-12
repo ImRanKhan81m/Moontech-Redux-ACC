d8662bab2daa3f14abc1fc7e0ba62843b19b047c
Md Imran Hossen <freelancerimran81@gmail.com>
2022-12-12 22:21:30 +0600

    code updated

--- MoonTech-Client/src/redux/reducers/productReducer.js
+++ MoonTech-Client/src/redux/reducers/productReducer.js
@@ -1,42 +1,78 @@
-import { ADD_TO_CART, REMOVE_FROM_CART } from "../actionTypes/actionTypes"
+import {
+  ADD_PRODUCT,
+  ADD_TO_CART,
+  PRODUCT_LOADED,
+  REMOVE_FROM_CART,
+  REMOVE_PRODUCT,
+} from "../actionTypes/actionTypes";
 
 const initialState = {
-    cart: []
-}
+  cart: [],
+  products: [],
+};
 
 const productReducer = (state = initialState, action) => {
-    const selectedProduct = state.cart.find(product => product._id === action.payload._id)
-    switch (action.type) {
+  const selectedProduct = state.cart.find(
+    (product) => product._id === action.payload._id
+  );
 
-        case ADD_TO_CART:
-            if (selectedProduct) {
-                const newCart = state.cart.filter(product => product._id !== selectedProduct._id)
-                selectedProduct.quantity = selectedProduct.quantity + 1;
-                return {
-                    ...state,
-                    cart: [...newCart, selectedProduct]
-                }
-            }
-            return {
-                ...state,
-                cart: [...state.cart, { ...action.payload, quantity: 1 }]
-            };
-        case REMOVE_FROM_CART:
-            if (selectedProduct.quantity > 1) {
-                const newCart = state.cart.filter(product => product._id !== selectedProduct._id)
-                selectedProduct.quantity = selectedProduct.quantity - 1;
-                return {
-                    ...state,
-                    cart: [...newCart, selectedProduct]
-                }
-            }
-            return {
-                ...state,
-                cart: state.cart.filter((item) => item._id !== action.payload._id)
-            };
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case ADD_PRODUCT:
+      return {
+        ...state,
+        products: [...state.products, action.payload],
+      };
+    case REMOVE_PRODUCT:
+      return {
+        ...state,
+        products: state.products.filter(
+          (product) => product._id !== action.payload
+        ),
+      };
+    case ADD_TO_CART:
+      if (selectedProduct) {
+        const newCart = state.cart.filter(
+          (product) => product._id !== selectedProduct._id
+        );
 
-export default productReducer
+        selectedProduct.quantity = selectedProduct.quantity + 1;
+
+        return {
+          ...state,
+          cart: [...newCart, selectedProduct],
+        };
+      }
+      return {
+        ...state,
+        cart: [...state.cart, { ...action.payload, quantity: 1 }],
+      };
+    case REMOVE_FROM_CART:
+      if (selectedProduct.quantity > 1) {
+        const newCart = state.cart.filter(
+          (product) => product._id !== selectedProduct._id
+        );
+        selectedProduct.quantity = selectedProduct.quantity - 1;
+
+        return {
+          ...state,
+          cart: [...newCart, selectedProduct],
+        };
+      }
+      return {
+        ...state,
+        cart: state.cart.filter(
+          (product) => product._id !== action.payload._id
+        ),
+      };
+
+    case PRODUCT_LOADED:
+      return {
+        ...state,
+        products: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default productReducer;
